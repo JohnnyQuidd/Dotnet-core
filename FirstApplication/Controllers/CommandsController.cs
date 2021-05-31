52ba8c150376fd2033d8e95ce36152312e470cfe
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-05-31 23:55:12 +0200

    Controller implementation

---
+++ FirstApplication/Controllers/CommandsController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using FirstApplication.data;
+using FirstApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : Controller
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            IEnumerable<Command> commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            Command command = _repository.GetCommandById(id);
+            return Ok(command);
+        }
+    }
+}
