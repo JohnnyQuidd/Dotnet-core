c271bc1df8a62306bfb355cb4715d2f3b73f8621
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-06-01 00:05:19 +0200

    Dependency Injection

--- FirstApplication/Controllers/CommandsController.cs
+++ FirstApplication/Controllers/CommandsController.cs
@@ -9,8 +9,13 @@ namespace FirstApplication.Controllers
     [ApiController]
     public class CommandsController : Controller
     {
-        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
-        
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
