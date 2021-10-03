a1eb9caafeb91b576e84957ae8a27ae51c2b452d
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-10-03 23:29:02 +0200

    Hardcoding CommandService URL in Controller

---
+++ Microservice-Project/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+	[Route("/api/c/[controller]")]
+	[ApiController]
+	public class PlatformsController : ControllerBase
+	{
+		public PlatformsController()
+		{
+
+		}
+
+		public ActionResult TestInboundConnection()
+		{
+			Console.WriteLine("--> Inbound connection");
+			return Ok("Inbound connection from Platforms Controller");
+		}
+	}
+}
---
+++ Microservice-Project/PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http
+{
+	public class HttpCommandDataClient : ICommandDataClient
+	{
+		private readonly HttpClient _httpClient;
+
+		public HttpCommandDataClient(HttpClient httpClient, IConfiguration confituration)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task SendPlatformToCommand(PlatformReadDto platform)
+		{
+			var httpContent = new StringContent(
+				JsonSerializer.Serialize(platform),
+				Encoding.UTF8,
+				"application/json"
+			);
+
+			var response = await _httpClient.PostAsync("http://localhost:6000/api/c/platforms", httpContent);
+
+			if (response.IsSuccessStatusCode)
+			{
+				Console.WriteLine("---> Sync POST to Command Service was OK");
+			}
+			else
+			{
+				Console.WriteLine("---> Sync POST to Command Service was NOT OK");
+			}
+			throw new NotImplementedException();
+		}
+	}
+}
