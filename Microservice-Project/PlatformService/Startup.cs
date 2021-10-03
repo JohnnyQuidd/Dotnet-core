53246e1102e55a95a3f56736ba36bff5886411ae
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-10-03 23:35:09 +0200

    Injecting Http Client

--- Microservice-Project/PlatformService/Startup.cs
+++ Microservice-Project/PlatformService/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService
 {
@@ -23,6 +24,7 @@ namespace PlatformService
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
             services.AddScoped<IPlatformRepo, PlatformRepo>();
+            services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
--- Microservice-Project/PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
+++ Microservice-Project/PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -29,7 +29,7 @@ namespace PlatformService.SyncDataServices.Http
 				"application/json"
 			);
 
-			var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}/api/c/platforms", httpContent);
+			var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}", httpContent);
 
 			if (response.IsSuccessStatusCode)
 			{
